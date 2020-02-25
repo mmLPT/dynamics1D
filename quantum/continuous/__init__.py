8e6fc68e478ba1494c90f6bb53137646a836bab0
LPTMartinezMaxime <martinez@irsamc.ups-tlse.fr>
2020-02-25 18:30:15 +0100

    classical + quantum

--- quantum/continuous/__init__.py
+++ quantum/continuous/__init__.py
@@ -1,5 +1,5 @@
 from dynamics1D.quantum.continuous.grid import *
 from dynamics1D.quantum.continuous.wavefunction import *
 from dynamics1D.quantum.continuous.operator import *
-# ~ from dynamics1D.quantum.continuous.husimi import *
+from dynamics1D.quantum.continuous.husimi import *
 
